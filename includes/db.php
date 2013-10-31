897f14f6c0087d9608215bdf9e1a4d36206f2dce
Ben <lumezious@googlemail.com>
2013-10-31 15:17:36 +0000

    Added connection check and some styling >:D
    Added connection check and some styling (much to Magnus' distaste)

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,6 @@
 <?php
 $db = new mysqli("comp2203.ecs.soton.ac.uk", "comp2203-lab5", "WL2133r1lM7lzp1706Ne", "comp2203-lab5");
+if ($db->connect_errno) {
+    echo "Failed to connect to MySQL: " . $db->connect_error;
+}
 ?>
